07fae262da8edc33a2f3f83365f45be69a3b684f
roman <rmir050607@gmail.com>
2022-03-24 21:00:21 +0300

    Added reverse into merge_sort function

--- lesson7_task2.py
+++ lesson7_task2.py
@@ -5,25 +5,33 @@
 
 import random
 
-def merge_sort(arr, left, right):
+def merge_sort(arr, left, right, reverse=False):
     if right - left > 1:
         mid = (left + right)//2
-        merge_sort(arr, left, mid)
-        merge_sort(arr, mid, right)
-        merge_list(arr, left, mid, right)
+        merge_sort(arr, left, mid, reverse)
+        merge_sort(arr, mid, right, reverse)
+        merge_list(arr, left, mid, right, reverse)
  
-def merge_list(arr, left_pos, mid_pos, right_pos):
+def merge_list(arr, left_pos, mid_pos, right_pos, reverse):
     left_arr = arr[left_pos:mid_pos]
     right_arr = arr[mid_pos:right_pos]
     k = left_pos
     i, j = 0, 0
     while (left_pos + i < mid_pos and mid_pos + j < right_pos):
-        if (left_arr[i] <= right_arr[j]):
-            arr[k] = left_arr[i]
-            i += 1
+        if reverse:
+            if left_arr[i] > right_arr[j]:
+                arr[k] = left_arr[i]
+                i += 1
+            else:
+                arr[k] = right_arr[j]
+                j += 1
         else:
-            arr[k] = right_arr[j]
-            j += 1
+            if left_arr[i] <= right_arr[j]:
+                arr[k] = left_arr[i]
+                i += 1
+            else:
+                arr[k] = right_arr[j]
+                j += 1
         k += 1
     if left_pos + i < mid_pos:
         while k < right_pos:
@@ -36,22 +44,23 @@ def merge_list(arr, left_pos, mid_pos, right_pos):
             j += 1
             k += 1
 
-m = 100000
+
+m = 50
 array = [random.randint(0, 49) for _ in range(m)]
 array_copy = array.copy()
 
-print(
-    'Исходный массив и отсортированный втроенной функцией массив равны?',
-    array == sorted(array_copy)
-    )
 if m < 100:
-    print(array)
+    print('Исходны массив:', array, sep='\n')
+    merge_sort(array, 0, len(array))
+    print('Отсортированный функцией merge_sort массив:', array, sep='\n')
+else:
+    print(
+        'Исходный массив и отсортированный втроенной функцией массив равны?',
+        array == sorted(array_copy)
+        )
+    merge_sort(array, 0, len(array))
+    print(
+        'Отсортированный функцией merge_sort массив и отсортированный втроенной функцией массив равны?',
+        array == sorted(array_copy)
+        )
 
-merge_sort(array, 0, len(array))
-
-print(
-    'Отсортированный функцией merge_sort массив и отсортированный втроенной функцией массив равны?',
-    array == sorted(array_copy)
-    )
-if m < 100:
-    print(array)
